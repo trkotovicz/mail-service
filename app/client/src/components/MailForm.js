9431227ef3c97c2803b104d06d8b72bea59a92bc
Thais Kotovicz <thais_kotovicz@hotmail.com>
2023-03-21 20:51:43 -0300

    feat: [client] mail form states created

--- app/client/src/components/MailForm.js
+++ app/client/src/components/MailForm.js
@@ -1,23 +1,42 @@
+import { useState } from 'react';
 import './MailForm.css';
 
 function MailForm() {
+  const [fields, setFields] = useState({
+    email: '',
+    name: '',
+    message: '',
+    file: ''
+  });
+
+  const handleInputChange = ({ target }) => {
+    if (target.name === 'file') fields[target.name] = target.files;
+    fields[target.name] = target.value;
+    setFields(fields);
+  }
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    console.log(fields);
+  }
+
   return (
     <main>
       <h1>E-Mail Service 📫 ✉️</h1>
       <form className="email-container">
       <label htmlFor="email">E-mail</label>
-      <input type="email" id="email" name="email" placeholder="type the email address" />
+      <input type="email" id="email" name="email" onChange={ handleInputChange } placeholder="type the email address" />
       
       <label htmlFor="name">Name</label>
-      <input type="text" id="name" name="name" placeholder="type your name" />
+      <input type="text" id="name" name="name" onChange={ handleInputChange } placeholder="type your name" />
       
       <label htmlFor="message">Message</label>
-      <textarea id="message" name="message" placeholder="type your message" />
+      <textarea id="message" name="message" onChange={ handleInputChange } placeholder="type your message" />
 
-      <label htmlFor="attach">Attach</label>
-      <input type="file" id="attach" name="attach" multiple />
+      <label htmlFor="file">Attach File</label>
+      <input type="file" id="file" name="file" onChange={ handleInputChange } multiple />
 
-      <button type="submit">Send</button>
+      <button type="submit" onClick={ handleClick }>Send</button>
       </form>
     </main>
   );
